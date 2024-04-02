5856a4eeadbc0bb93777a30f9caf2360a1fc49b2
GaberČuješ <gabercujes@gmail.com>
2024-04-02 12:04:46 +0200

    Register page and routing

---
+++ src/pages/Register/register.tsx
@@ -0,0 +1,16 @@
+import { Fragment } from "react/jsx-runtime";
+import './register.css'
+
+function register() {
+  return (
+    <Fragment>
+      <div className="container text-center">
+        <div className="row">
+          <div className="col"><img src="src/assets/images/login.png"></img></div>
+          <div className="col">Column</div>
+        </div>
+      </div>
+    </Fragment>
+  );
+}
+export default register;
