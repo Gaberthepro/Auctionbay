6782e381cab53ab9f980418bb919a42df682a545
GaberČuješ <gabercujes@gmail.com>
2024-04-04 11:12:08 +0200

    Login form design

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,17 @@
 import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Register from "./pages/Register/register";
-
+import Login from "./pages/Login/login";
 
 function App() {
-
   return (
     <BrowserRouter>
-    <Routes>
-      <Route path="/" element={""} />
-      <Route path="/Login" element={""} />
-      <Route path="/Register" element={<Register />} />
-    </Routes>
-  </BrowserRouter>
+      <Routes>
+        <Route path="/" element={""} />
+        <Route path="/Login" element={<Login />} />
+        <Route path="/Register" element={<Register />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- src/pages/Register/register.tsx
+++ src/pages/Register/register.tsx
@@ -1,13 +1,130 @@
 import { Fragment } from "react/jsx-runtime";
-import './register.css'
+import "./register.css";
 
 function register() {
   return (
     <Fragment>
       <div className="container text-center">
         <div className="row">
-          <div className="col"><img src="src/assets/images/login.png"></img></div>
-          <div className="col">Column</div>
+          <div className="col-7">
+            <img src="src/assets/images/login.png"></img>
+          </div>
+          <div id="login" className="col-5">
+            <div className="logo">
+              <img
+                src="src/assets/images/logo.png"
+                alt="Avatar"
+                className="avatar"
+              />
+            </div>
+            <div className="Hello">
+              <h1>Hello!</h1>
+              <p>Please enter your details</p>
+            </div>
+            <form>
+              <div className="name-label">
+                <label
+                  htmlFor="exampleFormControlInput1"
+                  className="form-label"
+                >
+                  Name
+                </label>
+                <label
+                  htmlFor="exampleFormControlInput1"
+                  className="form-label"
+                >
+                  Surname
+                </label>
+              </div>
+              <div className="name-input">
+                <input
+                  type="text"
+                  className="form-control"
+                  placeholder="Name"
+                  aria-label="Name"
+                />
+                <input
+                  type="text"
+                  className="form-control"
+                  placeholder="Surname"
+                  aria-label="Surname"
+                />
+              </div>
+
+              <div className="inputs">
+                <div className="email-label">
+                  <label
+                    htmlFor="exampleFormControlInput1"
+                    className="form-label"
+                  >
+                    Email
+                  </label>
+                </div>
+
+                <div className="other-inputs">
+                  <input
+                    type="email"
+                    className="form-control"
+                    id="exampleFormControlInput1"
+                    placeholder="Email"
+                  />
+                </div>
+              </div>
+
+              <div className="inputs">
+                <div className="email-label">
+                  <label
+                    htmlFor="exampleFormControlInput1"
+                    className="form-label"
+                  >
+                    Password
+                  </label>
+                </div>
+
+                <div className="other-inputs">
+                  <input
+                    type="password"
+                    className="form-control"
+                    id="exampleFormControlInput1"
+                    placeholder="Password"
+                  />
+                </div>
+              </div>
+
+              <div className="inputs">
+                <div className="email-label">
+                  <label
+                    htmlFor="exampleFormControlInput1"
+                    className="form-label"
+                  >
+                    Reapet Password
+                  </label>
+                </div>
+
+                <div className="other-inputs">
+                  <input
+                    type="password"
+                    className="form-control"
+                    id="exampleFormControlInput1"
+                    placeholder="Password"
+                  />
+                </div>
+              </div>
+              <div id="submit" className="d-grid gap-2 col-8 mx-auto">
+                <button className="btn btn-warning" type="submit">
+                  Sign up
+                </button>
+              </div>
+            </form>
+            <div className="toLogin">
+              <p>
+                Already have an account?{" "}
+                <a className="link-opacity-100" href="/Login">
+                  Create an account
+                </a>
+              </p>
+            </div>
+          </div>
         </div>
       </div>
     </Fragment>
