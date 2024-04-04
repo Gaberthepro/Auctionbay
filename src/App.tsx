f4a3daadec2cf807e3c87e365053ee78a491dfad
GaberČuješ <gabercujes@gmail.com>
2024-04-04 23:48:35 +0200

    Landing page

--- src/App.tsx
+++ src/App.tsx
@@ -3,17 +3,16 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Register from "./pages/Register/register";
 import Login from "./pages/Login/login";
 import Card from "./components/auciton-card/auction-card";
-import Navbar from "./components/navbar/navbar";
+import Home from "./pages/Home/home";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={""} />
+        <Route path="/" element={<Home />} />
         <Route path="/Login" element={<Login />} />
         <Route path="/Register" element={<Register />} />
         <Route path="/card" element={<Card />} />
-        <Route path="/navbar" element={<Navbar />} />
       </Routes>
     </BrowserRouter>
   );
