cbebcae71eb089fbc72c62b76febd3c5d1e1c81a
GaberČuješ <gabercujes@gmail.com>
2024-04-18 14:33:13 +0200

    Show my auctions

--- src/pages/MyAuctions/myAuctions.tsx
+++ src/pages/MyAuctions/myAuctions.tsx
@@ -1,13 +1,31 @@
 import { Fragment } from "react/jsx-runtime";
-import UserLocalStored from "../../services/localStoredData";
+import Card, { Auction } from "../../components/auciton-card/auction-card";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 export function MyAuctions() {
   const user_id = localStorage.getItem("user_id");
-  const user_data = UserLocalStored();
+
+  const [myAuctions, setMyAuctions] = useState<Auction[]>([]);
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:3000/auctions/me/" + user_id)
+      .then((response) => {
+        setMyAuctions(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, [user_id]);
 
   return (
     <Fragment>
-      <h1>My Auctions</h1>
+      <div className="cards-grid-container" style={{ marginTop: "1rem" }}>
+        {myAuctions.map((auction) => (
+          <Card key={auction.id} auction={auction} />
+        ))}
+      </div>
     </Fragment>
   );
 }
