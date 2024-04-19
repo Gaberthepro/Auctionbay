133a57b52422d0cc1f84238fe9062a771f0452d3
GaberČuješ <gabercujes@gmail.com>
2024-04-19 23:17:01 +0200

    set variables

--- src/pages/Auction/auction.tsx
+++ src/pages/Auction/auction.tsx
@@ -4,24 +4,39 @@ import "./auction.css";
 import { Auction } from "../../components/auciton-card/auction-card";
 import { useParams } from "react-router-dom";
 import axios from "axios";
+import { useEffect, useState } from "react";
 
 export function AuctionPage() {
   let { id } = useParams();
   let auction: Auction;
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const [imgURl, setImgURl] = useState("");
+  const [price, setPrice] = useState(0);
+  const [auction_user_id, setAuctionUserId] = useState(0);
 
-  axios
-    .get("http://localhost:3000/auctions/one/" + id)
-    .then((response) => {
-      auction = response.data;
-      console.log(auction);
-    })
-    .catch((error) => {
-      console.log(error);
-    });
+  useEffect(() => {
+    axios
+      .get("http://localhost:3000/auctions/one/" + id)
+      .then((response) => {
+        auction = response.data;
+        setTitle(auction.title);
+        setDescription(auction.description);
+        setImgURl(auction.imgURl);
+        setPrice(auction.starting_price);
+        setAuctionUserId(auction.user.id);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  });
 
   return (
     <Fragment>
-      <h1>This is auction page</h1>
+      <h1>{title}</h1>
+      <h2>{description}</h2>
+      <img src={imgURl}></img>
+      <h2>{price}</h2>
     </Fragment>
   );
 }
